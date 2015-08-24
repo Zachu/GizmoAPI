8c74b297af0ebc36837db558dbf7f3c3dfaaf281
Jani Korhonen <zachu@thegroup.fi>
2015-08-24 14:02:51 +0300

    Add notifications to gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
 var phpspec = require('gulp-phpspec');
+var notify = require('gulp-notify');
 
 gulp.task('test', function() {
   gulp.src('spec/**/*.php')
-    .pipe(phpspec())
+    .pipe(phpspec('', { notify: true })).
+    on('error', notify.onError({
+        title: 'Crap',
+        message: 'Tests failed'
+    }))
+    .pipe(notify({
+        title: 'Success',
+        message: 'All tests passed',
+        onLast: true
+    }));
 });
 
 gulp.task('watch', function() {
