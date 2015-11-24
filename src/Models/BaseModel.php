fa5a267f14ec7793dfabdc25895a21b0de7fdb0a
Jani Korhonen <zachu@thegroup.fi>
2015-11-24 17:08:59 +0200

    Add model constructor to BaseModel instead of models

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -1,60 +1,117 @@
 <?php namespace Pisa\Api\Gizmo\Models;
 
+use Pisa\Api\Gizmo\Adapters\HttpClientAdapter as HttpClient;
+
 abstract class BaseModel implements BaseModelInterface
 {
     use \Pisa\Api\Gizmo\Contracts\AttributableTrait;
     use \Pisa\Api\Gizmo\Contracts\IdentifiableTrait;
 
-    // @todo how do these work with eachother? If a field isn't fillable nor guarded, or if it's both?
     /**
-     * @var $fillable
-     * @todo  needed? used even?
      * Let these attributes be filled any time
+     * @var array
+     * @todo  needed? used even?
      */
     protected $fillable = [];
 
     /**
+     * Don't let these be filled unless its an empty field
      * @var $guarded
      * @todo  needed? used even?
-     * Don't let these be filled unless its an empty field
      */
     protected $guarded = [];
 
+    /**
+     * Attributes that are already sent to the service
+     * @var array
+     * @internal
+     */
     protected $savedAttributes = [];
 
+    /** @ignore */
+    protected $client;
+
     /**
-     * Used with method save(). Use that when creating a new model.
+     * [__construct description]
+     * @param HttpClient $client     [description]
+     * @param array      $attributes [description]
+     */
+    public function __construct(HttpClient $client, array $attributes = array())
+    {
+        $this->client = $client;
+        $this->load($attributes);
+    }
+
+    /**
+     * Create a new model instance.
+     *
+     * @internal  Use $this->save() for really creating a new model.
+     * @return BaseModel Return $this for chaining.
      */
     abstract protected function create();
 
     /**
-     * Used with method save(). Use that when updating your model.
+     * Update the model instance.
+     *
+     * @internal  Use $this->save() for really creating a new model.
+     * @return BaseModel Return $this for chaining.
      */
     abstract protected function update();
 
+    /**
+     * Delete the model instance
+     * @return BaseModel Return $this for chaining.
+     */
     abstract public function delete();
 
+    /**
+     * Load model attributes and mark them as saved.
+     * @param  array  $attributes Attributes to be loaded
+     * @return void
+     */
     public function load(array $attributes)
     {
         $this->fill($attributes);
         $this->savedAttributes = $this->attributes;
     }
 
+    /**
+     * Check which attributes are changed but not saved
+     * @return array Array of chanced attributes
+     */
     protected function changed()
     {
         return array_diff_assoc($this->attributes, $this->savedAttributes);
     }
 
+    /**
+     * Check if model exists.
+     *
+     * Checks that the primary key is set and is not empty.
+     *
+     * @return boolean
+     */
     public function exists()
     {
         return (isset($this->{$this->primaryKey}) && $this->{$this->primaryKey});
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * {@inheritDoc}
+     * @todo How the created/uncreated should be handled?
+     */
     public function isSaved()
     {
         return (empty($this->changed()) && $this->exists());
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * {@inheritDoc}
+     */
     public function save()
     {
         $return = null;
@@ -73,6 +130,12 @@ abstract class BaseModel implements BaseModelInterface
         return $return;
     }
 
+    /**
+     * Check if key is fillable
+     * @param  string  $key Key to check
+     * @return boolean
+     * @todo Check how fillable should work with guarded. Or if them both is even needed
+     */
     protected function isFillable($key)
     {
         $exists   = (isset($this->attributes[$key]));
@@ -82,7 +145,13 @@ abstract class BaseModel implements BaseModelInterface
         return ($fillable || (!$exists && $guarded));
     }
 
-    protected function toBool($var)
+    /**
+     * Convert all possible strings that the service might produce to php boolean
+     * @internal Intended for models internal usage
+     * @param  mixed   $var string to be converted
+     * @return boolean
+     */
+    protected static function toBool($var)
     {
         if (!is_string($var)) {
             return (bool) $var;
