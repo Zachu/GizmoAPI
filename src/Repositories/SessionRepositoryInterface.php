2f93c77b596605e4e7f5bf05d66245640f8b24ad
Jani Korhonen <zachu@thegroup.fi>
2015-12-16 17:12:58 +0200

    Fix phpdoc comments

--- src/Repositories/SessionRepositoryInterface.php
+++ src/Repositories/SessionRepositoryInterface.php
@@ -10,6 +10,7 @@ interface SessionRepositoryInterface extends BaseRepositoryInterface
      * @param  integer $skip          Skip number of instances (i.e. fetch the next page). Defaults to 0.
      * @param  string  $orderBy       Column to order the results with.
      * @return array                  List of active sessions filtered by criteria
+     * @api
      */
     public function findActiveBy(array $criteria, $caseSensitive = false, $limit = 30, $skip = 0, $orderby = null);
 
@@ -21,6 +22,7 @@ interface SessionRepositoryInterface extends BaseRepositoryInterface
      * @param  integer $skip          Skip number of instances (i.e. fetch the next page). Defaults to 0.
      * @param  string  $orderBy       Column to order the results with.
      * @return array                  List of active sessions filtered by criteria
+     * @api
      */
     public function findActiveInfosBy(array $criteria, $caseSensitive = false, $limit = 30, $skip = 0, $orderby = null);
 
@@ -28,7 +30,8 @@ interface SessionRepositoryInterface extends BaseRepositoryInterface
      * Finds one active session by criteria
      * @param  array   $criteria      Array of criteria to search for.
      * @param  boolean $caseSensitive Search for case sensitive parameters. Defaults to false.
-     * @return array                  One active session filtered by criteria
+     * @return array|null             One active session. Null if nothing found.
+     * @api
      */
     public function findOneActiveBy(array $criteria, $caseSensitive = false);
 
@@ -36,19 +39,24 @@ interface SessionRepositoryInterface extends BaseRepositoryInterface
      * Find one active session with additional information by criteria
      * @param  array   $criteria      Array of criteria to search for.
      * @param  boolean $caseSensitive Search for case sensitive parameters. Defaults to false.
-     * @return array                  One active session filtered by criteria
+     * @return array|null             One active session. Null if nothing found.
+     * @api
      */
     public function findOneActiveInfosBy(array $criteria, $caseSensitive = false);
 
     /**
      * Get all active sessions
      * @return array
+     * @api
      */
     public function getActive($limit = 30, $skip = 0, $orderby = null);
 
     /**
      * Get all active sessions and additional information
+     * @param  array   $criteria      Array of criteria to search for.
+     * @param  boolean $caseSensitive Search for case sensitive parameters. Defaults to false.
      * @return array
+     * @api
      */
     public function getActiveInfos($limit = 30, $skip = 0, $orderby = null);
 }
